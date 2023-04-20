696758e4deb9980d16e055dcb6e1227a93892e5b
Nicholas Tan <nicholas.tan01@gmail.com>
2023-04-20 12:15:10 -0400

    Hello world

---
+++ HelloWorldSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace HelloWorldSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is");
+            Console.ReadLine();
+        }
+    }
+}
